169ae9fbd5c9b42128fe4c687dbb8c30b494692d
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-02 10:36:55 +0300

    deleted rapid-api-key

--- config_data/config.py
+++ config_data/config.py
@@ -7,7 +7,6 @@ else:
     load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-RAPID_API_KEY = os.getenv('RAPID_API_KEY')
 DEFAULT_COMMANDS = (
     ('start', "Запустить бота"),
     ('help', "Вывести справку"),
