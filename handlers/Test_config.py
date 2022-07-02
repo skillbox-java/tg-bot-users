3e886d3eeef5267a06a66d569e1b4700a673b8d3
Vladimir Kireev <kireevvv@icloud.com>
2022-07-02 23:30:17 +0300

    test_confic created and fill

---
+++ handlers/Test_config.py
@@ -0,0 +1,11 @@
+from loader import bot
+@bot.message_handler(content_types=['text'])
+
+def get_text_messages(message):
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, 'Напиши привет')
+    else:
+        bot.send_message(message.from_user.id, 'Я не понимаю. Напиши /help.')
+
