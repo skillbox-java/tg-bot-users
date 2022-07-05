8cc547b017ec1a8fffac08c39d2e2606b724eec5
Vladimir Kireev <kireevvv@icloud.com>
2022-07-05 13:44:18 +0300

    aded functin check new_member and send helo

--- handlers/Test_config.py
+++ handlers/Test_config.py
@@ -6,6 +6,15 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
     elif message.text == '/help':
         bot.send_message(message.from_user.id, 'Напиши привет')
-    else:
-        bot.send_message(message.from_user.id, 'Я не понимаю. Напиши /help.')
+    # else:
+    #     bot.send_message(message.from_user.id, 'Я не понимаю. Напиши /help.')
+
+@bot.message_handler(content_types=['new_chat_members'])
+
+def handler_new_member(message):
+    # user_name = message.new_chat_member.first_name
+    user_name = message.from_user.first_name
+
+    bot.send_message(message.chat.id, 'Добро пожаловать, {0}'.format(user_name))
+
 
