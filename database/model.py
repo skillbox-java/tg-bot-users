86b7858e0b354ff7ea44b816ccd3ad14114de2c4
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-06 22:08:24 +0300

    added instructions for insert\select to db

--- database/model.py
+++ database/model.py
@@ -1,1 +1,19 @@
 # Здесь прописываем модель для базы данных пользователей
+import sqlite3
+
+
+
+with sqlite3.connect(('nvn.db')) as conn:
+    cursor = conn.cursor()
+    nickname = input('nickname: ')
+    user_name = input('user_name: ')
+    chat_name = input('chat_name: ')
+    cursor.execute("""
+    INSERT INTO 'users' (nickname, user_name, chat_name) VALUES (?, ?, ?);
+    """, (nickname, user_name, chat_name))
+
+
+
+    cursor.execute("SELECT * FROM 'users'")
+    result = cursor.fetchall()
+    print(result)
