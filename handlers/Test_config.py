207a9d784fe0a381b1bf229674a15a946a40b0af
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-06 12:53:14 +0300

    cosmetic changes

--- handlers/Test_config.py
+++ handlers/Test_config.py
@@ -1,6 +1,7 @@
 from loader import bot
-@bot.message_handler(content_types=['text'])
 
+
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == 'Привет':
         bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
@@ -9,12 +10,9 @@ def get_text_messages(message):
     # else:
     #     bot.send_message(message.from_user.id, 'Я не понимаю. Напиши /help.')
 
-@bot.message_handler(content_types=['new_chat_members'])
 
+@bot.message_handler(content_types=['new_chat_members'])
 def handler_new_member(message):
-    # user_name = message.new_chat_member.first_name
     user_name = message.from_user.first_name
 
     bot.send_message(message.chat.id, 'Добро пожаловать, {0}'.format(user_name))
-
-
