d8eb50ed9c5daa504b5fc038e7e6f07e65167c14
Vladimir Kireev <kireevvv@icloud.com>
2022-07-08 14:06:44 +0300

    added count members check

--- handlers/Test_config.py
+++ handlers/Test_config.py
@@ -16,14 +16,19 @@ def get_text_messages(message):
     #     bot.send_message(message.from_user.id, 'Я не понимаю. Напиши /help.')
 
 
+
 @bot.message_handler(content_types=['new_chat_members'])
 def handler_new_member(message):
     user_name = message.from_user.first_name
-    user_id = message.chat.id
-    insert_lite(nickname=message.from_user.username, user_name=message.from_user.first_name, chat_name=message.chat.title)
-    result = select()
-    print(result)
-    bot.send_message(message.chat.id, 'Добро пожаловать, {0}'.format(user_name))
+    # user_id = message.chat.id я забыл зачем это было нужно
+    count = bot.get_chat_members_count(message.chat.id)
+    if count == 6:
+        bot.send_message(message.chat.id, 'Нас включая бота, {0}'.format(count))
+    #
+    # insert_lite(nickname=message.from_user.username, user_name=message.from_user.first_name, chat_name=message.chat.title)
+    # result = select()
+    # print(result)
+    # bot.send_message(message.chat.id, 'Добро пожаловать, {0}'.format(user_name))
 
 
     # connect = sqlite3.connect('users.db')
