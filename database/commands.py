0250329c7b4099b8b8f197ee9829bc04b0be428f
Nikolay Kriuchkov <nickolaswux@gmail.com>
2022-07-08 17:57:57 +1200

    testing lucky_list.py lite_insert() in commands.py update nvn.db

--- database/commands.py
+++ database/commands.py
@@ -4,13 +4,19 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB = os.path.join(ROOT_DIR, 'nvn.db')
 
-def insert(nickname: str, user_name: str, chat_name: str) -> None:
+def insert(nickname: str, user_name: str, chat_name: str, user_number: int, dtime:str) -> None:
+    with sqlite3.connect((DB)) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO 'users' (nickname, user_name, chat_name, user_number, dtime_connetion) VALUES (?, ?, ?, ?,?);
+        """, (nickname, user_name, chat_name, user_number, dtime))
+
+def insert_lite(nickname: str, user_name: str, chat_name: str) -> None:
     with sqlite3.connect((DB)) as conn:
         cursor = conn.cursor()
         cursor.execute("""
         INSERT INTO 'users' (nickname, user_name, chat_name) VALUES (?, ?, ?);
         """, (nickname, user_name, chat_name))
-
 
 def select():
     with sqlite3.connect(( DB )) as conn:
