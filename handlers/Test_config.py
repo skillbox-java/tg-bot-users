7850729c06152670f65bd972aef9f4a099c0ffa9
Vladimir Kireev <kireevvv@icloud.com>
2022-07-08 18:38:57 +0300

    remove hello function from test_config

--- handlers/Test_config.py
+++ handlers/Test_config.py
@@ -5,18 +5,6 @@ from database.commands import insert_lite
 from database.commands import select
 
 
-
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == 'Привет':
-        bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
-    elif message.text == '/help':
-        bot.send_message(message.from_user.id, 'Напиши привет')
-    # else:
-    #     bot.send_message(message.from_user.id, 'Я не понимаю. Напиши /help.')
-
-
-
 @bot.message_handler(content_types=['new_chat_members'])
 def handler_new_member(message):
     nickname = message.from_user.username
