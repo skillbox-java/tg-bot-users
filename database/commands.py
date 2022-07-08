a887a07208989310bb4d1b3ba9037ef315dd8d6b
Vladimir Kireev <kireevvv@icloud.com>
2022-07-08 09:34:04 +0300

    test database by vladimir

--- database/commands.py
+++ database/commands.py
@@ -4,9 +4,8 @@ import sqlite3
 def insert(nickname: str, user_name: str, chat_name: str) -> None:
     with sqlite3.connect(('nvn.db')) as conn:
         cursor = conn.cursor()
-        cursor.execute("""
-        INSERT INTO 'users' (nickname, user_name, chat_name) VALUES (?, ?, ?);
-        """, (nickname, user_name, chat_name))
+        cursor.execute(""" INSERT INTO 'users' (nickname, user_name, chat_name) 
+        VALUES (nickname, user_name, chat_name) """)
 
 
 def select():
