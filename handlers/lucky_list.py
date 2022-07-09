ce0ed8aa0fb1ed8c12c81691291ced84df8da8cd
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-09 11:43:55 +0300

    no changes

--- handlers/lucky_list.py
+++ handlers/lucky_list.py
@@ -3,6 +3,7 @@ from loader import bot
 import database.commands as usersbase
 import emoji
 
+
 winners = usersbase.select()
 colomns =['Название группы', 'Имя участника', 'Ник участника', 'Порядковый номер вступления', 'Время вступления']
 # print(emoji.emojize('Python is :thumbs_up:'))
@@ -10,6 +11,7 @@ colomns =['Название группы', 'Имя участника', 'Ник 
 for winner in winners:
     print(emoji.emojize(f':party popper:{winner[3]} :person raising hand:{winner[2]} ({winner[1]}),'
                         f' \n :	input numbers:{winner[4]} :one o’clock:{winner[5]}'))
+
 
 @bot.message_handler(commands=['luckylist'])
 def bot_lucky_list(message: Message):
