c0bac78f66729b38c41985887f3f08849897e457
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-11 21:25:17 +0300

    merged Natasha & Kolya changes

--- database/commands.py
+++ database/commands.py
@@ -5,14 +5,15 @@ import datetime
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB = os.path.join(ROOT_DIR, 'nvn v2.db')
-MODERATOR_ID = ''
-print(MODERATOR_ID, 'before')
+
+
 def insert(nickname: str, user_name: str, chat_name: str, user_number: int, dtime=datetime.datetime.now().isoformat()) -> None:
     with sqlite3.connect((DB)) as conn:
         cursor = conn.cursor()
         cursor.execute("""
         INSERT INTO 'users' (nickname, user_name, chat_name, user_number, dtime_connetion) VALUES (?, ?, ?, ?,?);
         """, (nickname, user_name, chat_name, user_number, dtime))
+
 
 def insert2(nickname: str, user_name: str, chat_id: str, user_number: int, dtime:datetime) -> None:
     with sqlite3.connect((DB)) as conn:
@@ -21,6 +22,7 @@ def insert2(nickname: str, user_name: str, chat_id: str, user_number: int, dtime
         INSERT INTO 'users' (nickname, user_name, chat_id, user_number, dtime_connetion) VALUES (?, ?, ?, ?,?);
         """, (nickname, user_name, chat_id, user_number, dtime))
 
+
 def select():
     with sqlite3.connect(( DB )) as conn:
         cursor = conn.cursor()
@@ -28,28 +30,26 @@ def select():
         result = cursor.fetchall()
         return result
 
-def select_lucky():
+
+def winner_check(id):
     with sqlite3.connect(( DB )) as conn:
         cursor = conn.cursor()
-        print(MODERATOR_ID, 'select1')
-        cursor.execute(f"""SELECT group_name, user_name, nickname, user_number, dtime_connetion
-                           FROM 'users' JOIN 'user_groups'
-                           ON chat_id = group_id AND moderator_id = abs({MODERATOR_ID});""")
-        print(MODERATOR_ID, 'select2')
-    result = cursor.fetchall()
-    print(result)
-    return result
+        cursor.execute(f"SELECT is_winer FROM users WHERE is_winer={id}")
+        result = cursor.fetchall()
+        return result
 
-# print(select_lucky())
-print(MODERATOR_ID, 'after')
-# print(select_lucky())
-# insert2('katy11', 'katy_p',123,1236,datetime.datetime.now())
-# # {НазваниеГруппы} {ИмяУчастника} ({НикУчастника}),
-#     # {порядковыйНомерВступления} {ВремяВступления}
-#     # “Java разработчик” Василий(ника нет),
-#     # 500 26.06.22 10: 56
-# vibo = select_date()
-# for i in vibo:
-#     dt = datetime.datetime.strptime(i[0], '%Y-%m-%d %H:%M:%S.%f')
-#     print(dt.strftime('%d.%m.%y %H:%M'))
 
+def insert_to_groups(group_id: int, moderator_id: int) -> None:
+    with sqlite3.connect((DB)) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO 'user_groups' (group_id, moderator_id) VALUES (?, ?);
+        """, (group_id, moderator_id))
+
+
+def select_from_groups():
+    with sqlite3.connect((DB)) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM 'user_groups'")
+        result = cursor.fetchall()
+        return result
