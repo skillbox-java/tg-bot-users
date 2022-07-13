7ab3494788ad119973c1f34c108a29b133298d0c
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-13 10:11:14 +0300

    amendments for admin commands

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,27 +2,34 @@ from telebot.types import Message, CallbackQuery
 from loader import bot
 import database.commands as usersbase
 from keyboards.inline import admin_keyboard
+from config_data.config import ADMIN_IDS
 
 
 @bot.message_handler(commands=['adminshow'])
-def bot_admin_show(message: Message):
+def bot_admin_show(message: Message) -> None:
     admin_info = usersbase.select_from_groups()
     bot.send_message(chat_id=message.chat.id, text="Текущие настройки админки в формате\n"
                                                    "id Группа модераторов - id Группа пользователей")
     for i_info in admin_info:
-        bot.send_message(chat_id=message.chat.id, text=f'{i_info[0]} ("{i_info[1]}") -   {i_info[2]}')
+        bot_text = f"{i_info[0][0]}: "
+        for j_info in i_info:
+            bot_text = f"{bot_text}{j_info[1]} "
+        bot.send_message(chat_id=message.chat.id, text=bot_text)
 
 
 @bot.message_handler(commands=['adminsetup'])
 def bot_admin_setup(message: Message):
-    msg = bot.send_message(chat_id=message.chat.id,
-                           text="Добро пожаловать в меню админки. Добавьте группы пользователей (ГП), из которых "
-                                "будут приходить уведомления в выбранную группу модераторов (ГМ). Формат ввода:\n"
-                                "{id_ГМ} {id_ГП_1} {id_ГП_2} {id_ГП_3}")
-    bot.register_next_step_handler(message=msg, callback=process_admin_input)
+    if str(message.from_user.id) in ADMIN_IDS:
+        # bot.send_message(message.chat.id, text="Я очистил все данные таблицы ")
+        # usersbase.truncate_groups()
+        msg = bot.send_message(chat_id=message.chat.id,
+                               text="Добро пожаловать в меню админки. Добавьте группы пользователей (ГП), из которых "
+                                    "будут приходить уведомления в выбранную группу модераторов (ГМ). Формат ввода:\n"
+                                    "{id_ГМ} {id_ГП_1} {id_ГП_2} {id_ГП_3}")
+        bot.register_next_step_handler(message=msg, callback=process_admin_input)
 
 
-def process_admin_input(message: Message):
+def process_admin_input(message: Message) -> None:
     groups = message.text.split(" ")
 
     for i_group in groups:
@@ -34,32 +41,32 @@ def process_admin_input(message: Message):
             return
 
     for i in range(len(groups) - 1):
-        usersbase.insert_to_groups(group_id=int(groups[0]), moderator_id=int(groups[i + 1]))
+        usersbase.insert_to_groups(moderator_id=int(groups[0]), group_id=int(groups[i + 1]))
 
     bot.send_message(chat_id=message.chat.id,
                      text="Сделано! Хотите добавить еще группы?",
                      reply_markup=admin_keyboard.yes_no_keyboard())
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "yes_admin")
+@bot.callback_query_handler(func=lambda call: call.data == "yes_admin_add")
 def callback_query(call: CallbackQuery) -> None:
     """
     Колбек, обрабатывайщий необходимость ввести еще группы в меню админки. Перенаправляет на обработчик
     process_admin_input.
-    :param CallbackQuery call: yes_admin
+    :param CallbackQuery call: yes_admin_add
     :return: None
     """
-    bot.edit_message_text(chat_id=call.message.chat.id,
+    msg = bot.edit_message_text(chat_id=call.message.chat.id,
                           message_id=call.message.message_id,
                           text="Формат ввода:\n {id_ГМ} {id_ГП_1} {id_ГП_2} {id_ГП_3}")
-    process_admin_input(call.message)
+    bot.register_next_step_handler(msg, process_admin_input)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "no_admin")
+@bot.callback_query_handler(func=lambda call: call.data == "no_admin_add")
 def callback_query(call: CallbackQuery) -> None:
     """
     Колбек, обрабатывайщий окончание ввода групп в меню админки.
-    :param CallbackQuery call: no_admin
+    :param CallbackQuery call: no_admin_add
     :return: None
     """
     bot.edit_message_text(chat_id=call.message.chat.id,
