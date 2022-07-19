b0b96763b04599573a0785082f53f0593d06cfee
Natalia Loskutova <loskutovanl@gmail.com>
2022-07-19 09:52:01 +0300

    fixed commands to be available only for moderator groups

--- handlers/default_heandlers/help.py
+++ handlers/default_heandlers/help.py
@@ -1,9 +1,19 @@
 from telebot.types import Message
 from config_data.config import DEFAULT_COMMANDS
 from loader import bot
+from database.commands import get_moderator_id
 
 
 @bot.message_handler(commands=['help'])
 def bot_help(message: Message):
-    text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS]
-    bot.reply_to(message, '\n'.join(text))
+    """
+    Хендлер, который выводит все доступные боту команды при команде /help.
+    :param Message message: /help
+    :return: None
+    """
+    moderator_ids = get_moderator_id()
+
+    if message.chat.id in moderator_ids:
+        text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS]
+        bot.send_message(chat_id=message.chat.id,
+                         text='\n'.join(text))
--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -1,8 +1,22 @@
 from telebot.types import Message
 from loader import bot
+from config_data.config import ADMIN_IDS
+from database.commands import get_moderator_id
 
 
 @bot.message_handler(commands=['start'])
 def bot_start(message: Message):
-    bot.reply_to(message, f"Привет, {message.from_user.full_name}!")
+    """
+    Хендлер, который приветствует пользователя и подсказывает, что делать при команде /start.
+    :param Message message: /start
+    :return: None
+    """
+    moderator_ids = get_moderator_id()
 
+    if (message.chat.id in moderator_ids) or (str(message.from_user.id) in ADMIN_IDS):
+        bot.send_message(chat_id=message.chat.id,
+                         text=f"Привет, {message.from_user.full_name}!\n"
+                              f"Чтобы узнать, что я могу, введи команду /help\n\n"
+                              f"При первом запуске администратору следует выставить настройки админки по "
+                              f"команде /adminsetup в личном чате со мной (доступ есть у пользователей с "
+                              f"такими user_id:\n{ADMIN_IDS})")
